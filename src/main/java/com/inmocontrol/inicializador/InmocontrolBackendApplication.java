3cd38c8450498e680f1339de00f5c730ee19136b
juanes <jaramillojuan245@gmail.com>
2026-04-25 12:08:47 -0500

    25/04 Agregando configuración inicial

---
+++ src/main/java/com/inmocontrol/inicializador/InmocontrolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inmocontrol.inicializador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmocontrolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmocontrolBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/inmocontrol/negocio/assembler/entidad/package-info.java
@@ -0,0 +1,1 @@
+package com.inmocontrol.negocio.assembler.entidad;
